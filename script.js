37201006ef893482b82577503283346828d8e27e
Ara <oladipoeyiara@gmail.com>
2021-02-22 19:26:46 -0500

    Changed Images on Carousel

--- script.js
+++ script.js
@@ -23,23 +23,52 @@ let currentButton = 0;
 const comments0 = document.querySelector(".firsttheme");
 const comments1 = document.querySelector(".secondtheme");
 const comments2 = document.querySelector(".thirdtheme");
-const backButton = document.querySelector(`.backarrow${currentButton}`);
-const frontButton = document.querySelector(`.frontarrow${currentButton}`);
+const backButton1 = document.querySelector(`.backarrow0`);
+const backButton2 = document.querySelector(`.backarrow1`);
+const backButton3 = document.querySelector(`.backarrow2`);
+const frontButton1 = document.querySelector(`.frontarrow0`);
+const frontButton2 = document.querySelector(`.frontarrow1`);
+const frontButton3 = document.querySelector(`.frontarrow2`);
 
 // backButton.addEventListener("click", function () {
 //   console.log("lol");
 // });
 
-frontButton.addEventListener("click", function () {
+function toggleChanges(currentComment, nextComment) {
+  currentComment.classList.remove("flex");
+  currentComment.classList.add("hidden");
+  nextComment.classList.remove("hidden");
+  nextComment.classList.add("flex");
+}
+
+function detoggleChanges(currentComment, backComment) {
+  currentComment.classList.add("hidden");
+  currentComment.classList.remove("flex");
+  backComment.classList.remove("hidden");
+  backComment.classList.add("flex");
+}
+
+frontButton1.addEventListener("click", function () {
+  toggleChanges(comments0, comments1);
   currentComment++;
-  console.log(currentComment);
-  if (currentComment === 1) {
-    console.log("lo");
-    comments0.classList.remove("flex");
-    comments0.classList.add("hidden");
-    comments1.classList.remove("hidden");
-    comments1.classList.add("flex");
-  }
+  currentButton++;
 });
 
-console.log("in");
+frontButton2.addEventListener("click", function () {
+  toggleChanges(comments1, comments2);
+  frontButton3.style.color = "gray";
+  currentComment++;
+  currentButton++;
+});
+
+backButton2.addEventListener("click", function () {
+  detoggleChanges(comments1, comments0);
+});
+
+backButton3.addEventListener("click", function () {
+  detoggleChanges(comments2, comments1);
+});
+
+if (currentComment === 0) {
+  backButton1.style.color = "gray";
+}
